3487b454d78f4f9b6148aa04fbf958e3c82792b0
Elena <elena@selfnet.org>
2018-11-03 11:06:11 +0200

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <p>My name is Elena</p>
+    <p>My name is Elena!</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
